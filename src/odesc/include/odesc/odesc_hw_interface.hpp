24a75ba1c24eb666516b3063262634ed6b712a48
West <westly.bouchard@gmail.com>
2025-08-07 22:08:50 -0600

    Preliminary implementation of setup functions

--- src/odesc/include/odesc/odesc_hw_interface.hpp
+++ src/odesc/include/odesc/odesc_hw_interface.hpp
@@ -5,17 +5,20 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <atomic>
+#include <thread>
+
 namespace odesc {
 
     using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-    class ODescHWInterface : public hardware_interface::ActuatorInterface {
+    class ODescHwInterface : public hardware_interface::ActuatorInterface {
         public:
 
-        ODescHWInterface();
+        ODescHwInterface();
 
         CallbackReturn on_init(const hardware_interface::HardwareInfo& info) override;
-        
+
         CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
         CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
         CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
@@ -29,6 +32,21 @@ namespace odesc {
 
         hardware_interface::return_type read(const rclcpp::Time& time, const rclcpp::Duration& period) override;
         hardware_interface::return_type write(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+
+        private:
+
+        CallbackReturn error(std::string msg);
+
+        void rx_thread_func();
+
+        // Interface parameters
+        int can_id;
+        std::string can_interface;
+
+        // Private variables
+        int bus_fd;
+        std::atomic<bool> rx_running;
+        std::thread rx_thread;
     };
 
 };
--- src/odesc/src/odesc_hw_interface.cpp
+++ src/odesc/src/odesc_hw_interface.cpp
@@ -1,37 +1,148 @@
-#include "../include/odesc/odesc_hw_interface.hpp"
+#include "odesc/odesc_hw_interface.hpp"
+
+#include <linux/can/raw.h>
+#include <net/if.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+#include <sstream>
 
 namespace odesc {
+    using namespace std;
 
-    ODescHWInterface::ODescHWInterface() {}
+    ODescHwInterface::ODescHwInterface() {}
 
-    CallbackReturn ODescHWInterface::on_init(const hardware_interface::HardwareInfo& info) {
+    CallbackReturn ODescHwInterface::on_init(const hardware_interface::HardwareInfo& info) {
         if (hardware_interface::ActuatorInterface::on_init(info) != CallbackReturn::SUCCESS) {
             return CallbackReturn::ERROR;
         }
+
+        can_id = stoi(info_.hardware_parameters["can_id"]);
+
+        if (can_id < 0 || can_id > 63) {
+            stringstream msg;
+            msg << "Can id must be non-negative and less than 63. Id is currently: " << can_id;
+
+            return error(msg.str());
+        }
+
+        // Default interface is 'can0'
+        can_interface = "can0";
+        if (info_.hardware_parameters.find("can_interface") != info_.hardware_parameters.end()) {
+
+            string interface = info_.hardware_parameters["can_interface"];
+
+            if (interface.length() >= static_cast<string::size_type>(IFNAMSIZ)) {
+                stringstream msg;
+                msg << "Can interface name cannot be longer than " << IFNAMSIZ << " characters";
+                
+                return error(msg.str());
+            }
+
+            can_interface = info_.hardware_parameters["can_interface"];
+        }
+
+        return CallbackReturn::SUCCESS;
     }
 
 
-    CallbackReturn ODescHWInterface::on_configure(const rclcpp_lifecycle::State& previous_state) {}
+    CallbackReturn ODescHwInterface::on_configure(const rclcpp_lifecycle::State& previous_state) {
+        // Obtain file descriptor
+        bus_fd = socket(PF_CAN, static_cast<int32_t>(SOCK_RAW), CAN_RAW);
 
-    CallbackReturn ODescHWInterface::on_activate(const rclcpp_lifecycle::State& previous_state) {}
+        if (bus_fd < 0) {
+            return error("Failed to open can socket");
+        }
 
-    CallbackReturn ODescHWInterface::on_deactivate(const rclcpp_lifecycle::State& previous_state) {}
+        // Set interface name
+        struct ifreq ifr;
+        (void)strncpy(&ifr.ifr_name[0U], can_interface.c_str(), can_interface.length() + 1U);
+        if (ioctl(bus_fd, static_cast<uint32_t>(SIOCGIFINDEX), &ifr) != 0) {
+            return error("Failed to set can socket name via ioctl()");
+        }
 
-    CallbackReturn ODescHWInterface::on_cleanup(const rclcpp_lifecycle::State& previous_state) {}
+        // Create address and bind socket to it
+        struct sockaddr_can addr;
+        addr.can_family = static_cast<decltype(addr.can_family)>(AF_CAN);
+        addr.can_ifindex = ifr.ifr_ifindex;
+
+        if (bind(bus_fd, reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr)) < 0) {
+            return error("Failed to bind can socket");
+        }
+
+        // Disable local message loopback
+        if (setsockopt(bus_fd, SOL_CAN_RAW, CAN_RAW_LOOPBACK, 0, sizeof(int)) != 0) {
+            return error("Failed to disable local loopback");
+        }
+
+        // Set filtering to only receive frames from the correct id
+        struct can_filter rfilter;
+        rfilter.can_id = can_id;
+        rfilter.can_mask = CAN_SFF_MASK;
+
+        if (setsockopt(bus_fd, SOL_CAN_RAW, CAN_RAW_FILTER, &rfilter, sizeof(rfilter)) != 0) {
+            return error("Failed to configure can filters");
+        }
+
+        // Set up receive thread
+        rx_running = true;
+        rx_thread = thread(&ODescHwInterface::rx_thread_func, this);
+
+        // TODO: Set AxisState to IDLE before ending the loop (just to make sure)
+        // Also potentially set configuration parameters
+
+        return CallbackReturn::SUCCESS;
+
+    }
+
+    CallbackReturn ODescHwInterface::on_activate(const rclcpp_lifecycle::State& previous_state) {}
+
+    CallbackReturn ODescHwInterface::on_deactivate(const rclcpp_lifecycle::State& previous_state) {}
+
+    CallbackReturn ODescHwInterface::on_cleanup(const rclcpp_lifecycle::State& previous_state) {
+        // Shut down rx thread
+        rx_running = false;
+        if (rx_thread.joinable()) {
+            rx_thread.join();
+        }
+
+        // Close socket
+        if (bus_fd >= 0) {
+            close(bus_fd);
+            bus_fd = -1;
+        }
+
+        return CallbackReturn::SUCCESS;
+    }
 
 
-    CallbackReturn ODescHWInterface::on_shutdown(const rclcpp_lifecycle::State& previous_state) {}
+    CallbackReturn ODescHwInterface::on_shutdown(const rclcpp_lifecycle::State& previous_state) {}
 
-    CallbackReturn ODescHWInterface::on_error(const rclcpp_lifecycle::State& previous_state) {}
+    CallbackReturn ODescHwInterface::on_error(const rclcpp_lifecycle::State& previous_state) {}
 
 
-    std::vector<hardware_interface::StateInterface> ODescHWInterface::export_state_interfaces() {}
+    vector<hardware_interface::StateInterface> ODescHwInterface::export_state_interfaces() {}
 
-    std::vector<hardware_interface::CommandInterface> ODescHWInterface::export_command_interfaces() {}
+    vector<hardware_interface::CommandInterface> ODescHwInterface::export_command_interfaces() {}
     
 
-    hardware_interface::return_type ODescHWInterface::read(const rclcpp::Time& time, const rclcpp::Duration& period) {}
+    hardware_interface::return_type ODescHwInterface::read(const rclcpp::Time& time, const rclcpp::Duration& period) {}
 
-    hardware_interface::return_type ODescHWInterface::write(const rclcpp::Time& time, const rclcpp::Duration& period) {}
+    hardware_interface::return_type ODescHwInterface::write(const rclcpp::Time& time, const rclcpp::Duration& period) {}
+
+
+    CallbackReturn ODescHwInterface::error(string msg) {
+        RCLCPP_ERROR_STREAM(rclcpp::get_logger("ODescHwInterface"), msg);
+        return CallbackReturn::ERROR;
+    }
+
+    void ODescHwInterface::rx_thread_func() {
+        while (rx_running) {
+            // Read frame
+        }
+    }
 
 };
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(odesc::ODescHwInterface, hardware_interface::ActuatorInterface)
